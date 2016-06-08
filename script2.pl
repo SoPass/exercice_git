58e6d3d32d5b9ae41c9765fc7aa58348532d6882
Sophie <sophie.passemar@yahoo.fr>
2016-06-08 13:55:37 +0200

    Ajout UTF8 et saut de ligne

--- script2.pl
+++ script2.pl
@@ -1,11 +1,12 @@
 use strict;
 use locale;
 use warnings;
+use utf8;
 
 while (my $ligne = <STDIN>){
 	chomp $ligne;
 
 	if ($ligne =~ /(.*)\t("_")\t(.*)/){
-		print $1 $2 $3
+		print $1 $2 $3, "\n";
 	}	
 }
